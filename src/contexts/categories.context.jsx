6aec83575abfa3a330a8ab61e26dd45f9f555a60
Saii3488 <saiipranav197@gmail.com>
2024-03-15 22:44:08 +0530

    converted categories fetch to graphql queries

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,23 +1,29 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
-
+import { gql,useQuery } from '@apollo/client';
+const COLLECTIONS = gql`
+  query GetCollections {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
 export const CategoriesProvider = ({ children }) => {
+  const{loading,error,data}=useQuery(COLLECTIONS)
   const [categoriesMap, setCategoriesMap] = useState({});
-
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
-
+  console.log("loading",loading);
+  console.log("data",data);
   const value = { categoriesMap };
   return (
     <CategoriesContext.Provider value={value}>
